f7407a7c6a604f788bf17ae151ba06c9f5270cd4
Stewart Leach <stewart.leach@isobar.com>
2018-06-07 10:36:33 +1000

    looping over csv data rows

--- main.go
+++ main.go
@@ -6,22 +6,19 @@ import (
 	"github.com/leachuk/datautils/mappify"
 )
 
+
+
 func main() {
 	csv := csvservice.ReadCSV("/csvservice/datasample.csv")
-	fmt.Printf("\nCSV:%s\n", csv)
 
-	address1 := mappify.Address{
-		csv[0].Data["streetAddress"],
-		csv[0].Data["suburb"],
-		csv[0].Data["postCode"],
-		csv[0].Data["state"]}
+	for i, row := range csv{
+		var address = mappify.Address{
+			row.Data["streetAddress"],
+			row.Data["suburb"],
+			row.Data["postCode"],
+			row.Data["state"]}
 
-	address2 := mappify.Address{
-		csv[1].Data["streetAddress"],
-		csv[1].Data["suburb"],
-		csv[1].Data["postCode"],
-		csv[1].Data["state"]}
+		fmt.Printf("Return address [%v]: %s\n",i, mappify.AddressGeocode(address))
+	}
 
-	fmt.Println("Return address 1: " + mappify.AddressGeocode(address1))
-	fmt.Println("Return address 2: " + mappify.AddressGeocode(address2))
 }
